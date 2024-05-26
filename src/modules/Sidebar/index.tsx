3ce1c1b5145849ffac75d905e1ea74063051c5cd
Xiaoqie <157262392+XiaoqieChujunhao@users.noreply.github.com>
2024-05-26 18:55:21 +0800

    Update index.tsx

--- src/modules/Sidebar/index.tsx
+++ src/modules/Sidebar/index.tsx
@@ -81,7 +81,7 @@ const Sidebar: FC<{
   return (
     <div className="flex flex-col h-full bg-white">
       <div className="p-4 mt-2 flex items-baseline justify-between">
-        <span className="text-3xl text-gradient font-[800]">QieStudy</span>
+        <span className="text-3xl text-gradient font-[800]">ChatGPT</span>
         <a
           href="https://github.com/XiaoqieChujunhao/chatgpt-vercel"
           target="_blank"
